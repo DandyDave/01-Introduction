bf74afd248175c334575b860ff1b449d30dbd12c
Myer <ndmyer@iu.edu>
2019-08-28 17:07:59 -0400

    Almost Done

--- main.py
+++ main.py
@@ -6,3 +6,15 @@ utils.check_version((3,7))
 utils.clear()
 
 print('Hello, Class!')
+
+print('My names Nathaniel Myer')
+
+print('This is hard because it is constantly changing but at the moment my favourite game is Dicey Dungeons, a turn based roughlike which relies on dice rolls.')
+
+print('I am concerned that since im taking 19 credits hours I will not be able to dedicate the time I want to in this class.')
+
+print('I am excited to get some more knowledge about Python since I enjoyed the language the little bit I used it in the class. I am also excited about what it will allow me create.')
+
+print('My stack overflow number is 11980670')
+
+print('My github is https://github.com/DandyDave')
