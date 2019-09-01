46f4d124a13fd047979e084fb02899570dc617cc
DandyDave <ndmyer@iu.edu>
2019-08-31 23:33:29 -0400

    finished the special code

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import sys
+import time
 import utils
 
 utils.check_version((3,7))
@@ -18,3 +20,19 @@ print('I am excited to get some more knowledge about Python since I enjoyed the 
 print('My stack overflow number is 11980670')
 
 print('My github is https://github.com/DandyDave')
+
+print("~~~~~~~~~~~~~~~~~~~~")
+print("| Hey, How Are You |")
+print("~~~~~~~~~~~~~~~~~~~~")
+
+time.sleep(3)
+
+choice = input("This is a little text aventure game do you want to play it? [y/n] ")
+
+time.sleep(2)
+
+if choice in ['YES','yes','Yes','y','Y']:
+    print("Well... I got this far and got a bit lazy so... this is it. I mean... i guess you win? Yeah?")
+else:
+    print("Wow!!! I tried so hard and you won't even give it a try. That figures.")
+    sys.exit()
